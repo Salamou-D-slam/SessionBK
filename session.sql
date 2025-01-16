8c63a11b6b0ff8af3ed37fb1b2ef220385bb3089
Islam <islamderrouiche@gmail.com>
2025-01-16 08:17:54 +0100

    modif deploiment

--- session.sql
+++ session.sql
@@ -5,8 +5,3 @@ CREATE TABLE users(
     email VARCHAR(100) NOT NULL UNIQUE,
     password VARCHAR (100)
 );
-
-INSERT INTO users
-VALUES (1, 'islam', 'derrouiche', 'islam@gmail.com', 'islam_session');
-INSERT INTO users
-VALUES (2, 'Josette', 'joseline', 'josette@gmail.com', 'josette_session')
